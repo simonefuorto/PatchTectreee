03fe2a0076a1506bbd811a2d254fe72203c20f57
Simone Fuorto <si.fuorto@studenti.unina.it>
2026-08-01 18:16:45 +0200

    fix: aggiunta mru_policy mancante in TARDISTSO_TECTREE.py

--- configs/ruby/TARDISTSO_TECTREE.py
+++ configs/ruby/TARDISTSO_TECTREE.py
@@ -126,6 +126,7 @@ def create_system(options, full_system, system, dma_ports, bootmem, ruby_system,
 
     for i, dir_cntrl in enumerate(dir_cntrl_nodes):
         dir_cntrl.lease = lease
+        dir_cntrl.mru_policy = options.mru_policy
         
         dir_cntrl.LLC = DirCache(
             size="2MB",
